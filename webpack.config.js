76f2dd09d31083376fc4aee19dbcbc5b5541cf8b
Davidski7 <U2486889@rts-365.dk>
2024-10-21 14:07:40 +0200

    Vi er færdige med opgaven

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+
+module.exports = {
+    entry: "./app/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                use: 'svg-inline-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [['@babel/preset-env',
+                            { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    plugins: [new HtmlWebpackPlugin()],
+    mode: "production"
+}
